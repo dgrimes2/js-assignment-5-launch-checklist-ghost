49ae928753b1df9b187f21a592a480059a4193eb
dgrimes2 <grime22d@mtholyoke.edu>
2023-08-14 22:23:20 -0500

    fixed list visibility

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
-    form.addEventListener("submit", function() {
+    let list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
+    form.addEventListener("submit", function(event) {
         event.preventDefault();
         //Use to interact with the shuttle status list on HTML/index and update
         let pilot = document.querySelector("input[name=pilotName]").value;
         let copilot = document.querySelector("input[name=copilotName]").value;
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
-        let list = document.getElementById("faultyItems");
 
         //use formsubmission() to validate and update list
         formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
